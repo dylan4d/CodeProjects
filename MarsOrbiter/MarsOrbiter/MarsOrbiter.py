6cc3b90c03db6a5ca7c4718bc017959b5c762bd7
dylan4d <72990303+dylan4d@users.noreply.github.com>
2022-08-15 19:37:29 +0100

    Add files via upload

--- MarsOrbiter/MarsOrbiter/MarsOrbiter.py
+++ MarsOrbiter/MarsOrbiter/MarsOrbiter.py
@@ -4,7 +4,6 @@
 import os
 import math
 import random
-from numpy import blackman
 import pygame as pg
 
 # color table
@@ -36,66 +35,66 @@ class Satellite(pg.sprite.Sprite):
         self.fuel = 100
         self.mass = 1
         self.distance = 0
+        self.thrust = pg.mixer.Sound('thrust_audio.ogg')
+        self.thrust.set_volume(0.07)
 
-# firing thrusters and checking for player input
-def thruster(self, dx, dy):
-    '''Execute actions associated with firing thrusters'''
-    self.dx += dx
-    self.dy += dy
-    self.fuel -= 2
-    self.thrust.play()
+    # firing thrusters and checking for player input
+    def thruster(self, dx, dy):
+        '''Execute actions associated with firing thrusters'''
+        self.dx += dx
+        self.dy += dy
+        self.fuel -= 2
+        self.thrust.play()
 
-def check_keys(self):
-    '''Check if user presses arrow keys and call thruster method'''
-    keys = pg.key.get_pressed()
+    def check_keys(self):
+        '''Check if user presses arrow keys and call thruster method'''
+        keys = pg.key.get_pressed()
 
-    # fire thrusters
-    if keys[pg.K_RIGHT]:
-        self.thruster(0.05, 0)
+        # fire thrusters
+        if keys[pg.K_RIGHT]:
+            self.thruster(0.05, 0)
+        elif keys[pg.K_LEFT]:
+            self.thruster(-0.05, 0)
+        elif keys[pg.K_UP]:
+            self.thruster(0, -0.05)
+        elif keys[pg.K_DOWN]:
+            self.thruster(0, 0.05)
 
-    if keys[pg.K_LEFT]:
-        self.thruster(-0.05, 0)
+    def locate(self, planet):
+        '''calculate distance and heading to planet'''
+        px, py = planet.x, planet.y
+        dist_x = self.x - px
+        dist_y = self.y - py
 
-    if keys[pg.K_UP]:
-        self.thruster(0, -0.05)
+        # get direction to planet to orient png
+        planet_dir_radians = math.atan2(dist_x, dist_y)
+        self.heading = planet_dir_radians * 180 / math.pi
+        self.heading -= 90
+        self.distance = math.hypot(dist_x, dist_y)
 
-    if keys[pg.K_DOWN]:
-        self.thruster(0, 0.05)
+    def rotate(self):
+        """Rotate satellite using degrees so dish faces planet."""
+        self.image = pg.transform.rotate(self.image_sat, self.heading)
+        self.rect = self.image.get_rect()
 
-def locate(self, planet):
-    '''calculate distance and heading to planet'''
-    px, py = planet.x, planet.y
-    dist_x = self.x - px
-    dist_y = self.y - py
+    def path(self):
+        """Update satellite's position & draw line to trace orbital path"""
+        last_center = (self.x, self.y)
+        self.x += self.dx
+        self.y += self.dy
+        pg.draw.line(self.background, WHITE, last_center, (self.x, self.y))
 
-    # get direction to planet to orient png
-    planet_dir_radians = math.atan2(dist_x, dist_y)
-    self.heading = planet_dir_radians * 180 / math.pi
-    self.heading -= 90
-    self.distance = math.hypot(dist_x, dist_y)
+    def update(self):
+        """Update satellite object during game"""
+        self.check_keys()
+        self.rotate()
+        self.path()
+        self.rect.center = (self.x, self.y)
+        # change image to fiery red if in atmosphere
+        if self.dx == 0 and self.dy == 0:
+            self.image = self.image_crash
+            self.image.set_colorkey(BLACK)
 
-def rotate(self):
-    """Rotate satellite using degrees so dish faces planet."""
-    self.image = pg.transform.rotate(self.image_sat, self.heading)
-    self.rect = self.image.get_rect()
-
-def path(self):
-    """Update satellite's position & draw line to trace orbital path"""
-    last_center = (self.x, self.y)
-    self.x += self.dx
-    self.y += self.dy
-    pg.draw.line(self.background, WHITE, last_center, (self.x, self.y))
-
-def update(self):
-    """Update satellite object during game"""
-    self.check_keys()
-    self.rotate()
-    self.path()
-    self.rect.center = (self.x, self.y)
-    # change image to fiery red if in atmosphere
-    if self.dx == 0 and self.dy == 0:
-        self.image = self.image_crash
-        self.image.set_colorkey(BLACK)
 class Planet(pg.sprite.Sprite):
     """Planet object that rotates & projects gravity field"""
 
@@ -113,4 +112,211 @@ class Planet(pg.sprite.Sprite):
         self.rect.center = (self.x, self.y)
         self.angle = math.degrees(0)
         self.rotate_by = math.degrees(0.01)
+
+
+    def rotate(self):
+        """Rotate the planet image with each game loop."""
+        last_center = self.rect.center
+        self.image = pg.transform.rotate(self.image_copy, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = last_center
+        self.angle += self.rotate_by
+
+    def gravity(self, satellite):
+        """Calculate impact of gravity on the satellite."""
+        G = 1.0  # gravitational constant for game
+        dist_x = self.x - satellite.x
+        dist_y = self.y - satellite.y
+        distance = math.hypot(dist_x, dist_y)     
+        # normalize to a unit vector
+        dist_x /= distance
+        dist_y /= distance
+        # apply gravity
+        force = G * (satellite.mass * self.mass) / (math.pow(distance, 2))
+        satellite.dx += (dist_x * force)
+        satellite.dy += (dist_y * force)
         
+    def update(self):
+        """Call the rotate method."""
+        self.rotate()
+
+def calc_eccentricity(dist_list):
+    """Calculate & return eccentricity from list of radii."""
+    apoapsis = max(dist_list)
+    periapsis = min(dist_list)
+    eccentricity = (apoapsis - periapsis) / (apoapsis + periapsis)
+    return eccentricity
+
+def instruct_label(screen, text, color, x, y):
+    """Take screen, list of strings, color, & origin & render text to screen."""
+    instruct_font = pg.font.SysFont(None, 25)
+    line_spacing = 22
+    for index, line in enumerate(text):
+        label = instruct_font.render(line, True, color, BLACK)
+        screen.blit(label, (x, y + index * line_spacing))
+
+def box_label(screen, text, dimensions):
+    """Make fixed-size label from screen, text & left, top, width, height."""
+    readout_font = pg.font.SysFont(None, 27)
+    base = pg.Rect(dimensions)
+    pg.draw.rect(screen, WHITE, base, 0)
+    label = readout_font.render(text, True, BLACK)
+    label_rect = label.get_rect(center=base.center)
+    screen.blit(label, label_rect)
+
+def mapping_on(planet):
+    """Show soil moisture image on Mars."""
+    last_center = planet.rect.center
+    planet.image_copy = pg.transform.scale(planet.image_water, (100, 100))
+    planet.image_copy.set_colorkey(BLACK)
+    planet.rect = planet.image_copy.get_rect()
+    planet.rect.center = last_center
+
+def mapping_off(planet):
+    """Restore normal planet image."""
+    planet.image_copy = pg.transform.scale(planet.image_mars, (100, 100))
+    planet.image_copy.set_colorkey(BLACK)
+
+def cast_shadow(screen):
+    """Add optional terminator & shadow behind planet to screen."""
+    shadow = pg.Surface((400, 100), flags=pg.SRCALPHA)  # tuple is w,h
+    shadow.fill((0, 0, 0, 210))  # last number sets transparency
+    screen.blit(shadow, (0, 270))  # tuple is top left coordinates
+
+def main():
+    """Set-up labels & instructions, create objects & run the game loop."""
+    pg.init()  # initialize pygame
+    
+    # set-up display
+    os.environ['SDL_VIDEO_WINDOW_POS'] = '700, 100'  # set game window origin
+    screen = pg.display.set_mode((800, 645), pg.FULLSCREEN) 
+    pg.display.set_caption("Mars Orbiter")
+    background = pg.Surface(screen.get_size())
+
+    # enable sound mixer
+    pg.mixer.init()
+
+    intro_text = [
+        ' The Mars Orbiter experienced an error during Orbit insertion.',
+        ' Use thrusters to correct to a circular mapping orbit without',
+        ' running out of propellant or burning up in the atmosphere.'
+        ]
+ 
+    instruct_text1 = [
+        'Orbital altitude must be within 69-120 miles',
+        'Orbital Eccentricity must be < 0.05',
+        'Avoid top of atmosphere at 68 miles'    
+        ]
+
+    instruct_text2 = [
+        'Left Arrow = Decrease Dx', 
+        'Right Arrow = Increase Dx', 
+        'Up Arrow = Decrease Dy', 
+        'Down Arrow = Increase Dy', 
+        'Space Bar = Clear Path',
+        'Escape = Exit Full Screen'        
+        ]  
+
+    # instantiate planet and satellite objects
+    planet = Planet()
+    planet_sprite = pg.sprite.Group(planet)
+    sat = Satellite(background)    
+    sat_sprite = pg.sprite.Group(sat)
+
+    # for circular orbit verification
+    dist_list = []  
+    eccentricity = 1
+    eccentricity_calc_interval = 5  # optimized for 120 mile altitude
+    
+    # time-keeping
+    clock = pg.time.Clock()
+    fps = 30
+    tick_count = 0
+
+    # for soil moisture mapping functionality
+    mapping_enabled = False
+    
+    running = True
+    while running:
+        clock.tick(fps)
+        tick_count += 1
+        dist_list.append(sat.distance)
+        
+        # get keyboard input
+        for event in pg.event.get():
+            if event.type == pg.QUIT:  # close window
+                running = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                screen = pg.display.set_mode((800, 645))  # exit full screen
+            elif event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                background.fill(BLACK)  # clear path
+            elif event.type == pg.KEYUP:
+                sat.thrust.stop()  # stop sound
+                mapping_off(planet)  # turn-off moisture map view
+            elif mapping_enabled:
+                if event.type == pg.KEYDOWN and event.key == pg.K_m:
+                    mapping_on(planet)
+
+        # get heading & distance to planet & apply gravity               
+        sat.locate(planet)  
+        planet.gravity(sat)
+
+        # calculate orbital eccentricity
+        if tick_count % (eccentricity_calc_interval * fps) == 0:
+            eccentricity = calc_eccentricity(dist_list)
+            dist_list = []              
+
+        # re-blit background for drawing command - prevents clearing path
+        screen.blit(background, (0, 0))
+        
+        # Fuel/Altitude fail conditions
+        if sat.fuel <= 0:
+            instruct_label(screen, ['Fuel Depleted!'], RED, 340, 195)
+            sat.fuel = 0
+            sat.dx = 2
+        elif sat.distance <= 68:
+            instruct_label(screen, ['Atmospheric Entry!'], RED, 320, 195)
+            sat.dx = 0
+            sat.dy = 0
+
+        # enable mapping functionality
+        if eccentricity < 0.05 and sat.distance >= 69 and sat.distance <= 120:
+            map_instruct = ['Press & hold M to map soil moisture']
+            instruct_label(screen, map_instruct, LT_BLUE, 250, 175)
+            mapping_enabled = True
+        else:
+            mapping_enabled = False
+
+        planet_sprite.update()
+        planet_sprite.draw(screen)
+        sat_sprite.update()
+        sat_sprite.draw(screen)
+
+        # display intro text for 15 seconds      
+        if pg.time.get_ticks() <= 15000:  # time in milliseconds
+            instruct_label(screen, intro_text, GREEN, 145, 100)
+
+        # display telemetry and instructions
+        box_label(screen, 'Dx', (70, 20, 75, 20))
+        box_label(screen, 'Dy', (150, 20, 80, 20))
+        box_label(screen, 'Altitude', (240, 20, 160, 20))
+        box_label(screen, 'Fuel', (410, 20, 160, 20))
+        box_label(screen, 'Eccentricity', (580, 20, 150, 20))
+        
+        box_label(screen, '{:.1f}'.format(sat.dx), (70, 50, 75, 20))     
+        box_label(screen, '{:.1f}'.format(sat.dy), (150, 50, 80, 20))
+        box_label(screen, '{:.1f}'.format(sat.distance), (240, 50, 160, 20))
+        box_label(screen, '{}'.format(sat.fuel), (410, 50, 160, 20))
+        box_label(screen, '{:.8f}'.format(eccentricity), (580, 50, 150, 20))
+          
+        instruct_label(screen, instruct_text1, WHITE, 10, 575)
+        instruct_label(screen, instruct_text2, WHITE, 570, 510)
+      
+        # add terminator & border
+        cast_shadow(screen)
+        pg.draw.rect(screen, WHITE, (1, 1, 798, 643), 1)
+
+        pg.display.flip()
+
+if __name__ == "__main__":
+    main()
