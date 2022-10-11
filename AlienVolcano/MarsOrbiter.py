8483778220ed21c0692e6ffef2614938042445c7
dylan4d <72990303+dylan4d@users.noreply.github.com>
2022-10-11 22:39:25 +0100

    Delete MarsOrbiter.py

--- AlienVolcano/MarsOrbiter.py
+++
@@ -1,3 +0,0 @@
-'''Use Pygame to build an arcade game that teaches the fundamentals of orbital mechanics. THe game's goal is to nudge a satellite into a circular mapping orbit without running out of fuel or burning up in the atmosphere.'''
-
-import
